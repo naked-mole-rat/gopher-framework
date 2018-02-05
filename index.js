15a1601024d674e447e1c32aea0e25dc982b1f2a
Philippe Schwyter <pschwyter90@gmail.com>
2018-02-04 19:13:34 -0800

    we can get files i guess

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 const Gopher = require('./src/gopher');
-module.exports = Gopher;
+
+const gopher = new Gopher();
+
+gopher.addRoute(
+  '',
+  async ctx => {
+    return ;
+  }
+);
+
+gopher.addRoute(
+  'darkest-secrets',
+  async ctx => {
+    return 'i love cheese';
+  }
+);
+
+gopher.listen(3000);
--- src/gopher.js
+++ src/gopher.js
@@ -5,27 +5,38 @@ class Gopher {
 
   constructor() {
     this.server = net.createServer(this._setupServer.bind(this));
+    this.routes = {};
+  }
+
+  addRoute(route, handler) {
+    this.routes[route] = async (ctx) => { 
+      return handler(ctx);
+    };
   }
 
   _setupServer(socket) {
-    socket.on('data', data => {
-      this._handleData(socket, data);
+    socket.on('data', buffer => {
+      const selector = buffer.toString().trim();
+
+      const gopherRequest = new GopherRequest(selector);
+      const context = {
+        request: gopherRequest,
+        socket: socket
+      };
+
+      this._handleRequest(context).then(() => {
+        socket.end();
+      });
     });
   }
 
-  _handleData(socket, data) {
-    const selector = data.toString().trim();
-    const gopherRequest = new GopherRequest(selector);
-    const context = {
-      request: gopherRequest,
-      socket: socket
+  async _handleRequest(context) {
+    const route = context.request.location;
+    if (!this.routes[route]) {
+      throw new Error('Not Found');
     }
-    this.requestCallback(context);
-  }
-
-  use(fn) {
-    // this all you get for now
-    this.requestCallback = fn;
+    const response = await this.routes[route](context);
+    context.socket.write(response);
   }
 
   listen(port = 80, ip = '127.0.0.1') {
